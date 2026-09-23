7017dcffdb2fe5bcd9a7107fd0d09e050227df90
jyoo414-web <jyoo414@gmail.com>
2026-09-23 17:43:35 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,20 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
-});
+{
+  "name": "밸런스 게임 + 술게임",
+  "short_name": "밸런스게임",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#121212",
+  "theme_color": "#121212",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
